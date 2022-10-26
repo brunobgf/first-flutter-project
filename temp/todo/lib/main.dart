58eacc5912ceb3c5072b37dd6177fd9dc037a764
brunobgf <brunobgf40@gmail.com>
2022-10-26 13:26:30 -0300

    first commit

---
+++ temp/todo/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Todo List"),
+        ),
+        body: Container(
+          child: Center(
+            child: Text("Hello world!"),
+          ),
+        ));
+  }
+}
